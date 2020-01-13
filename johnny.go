36a9b24bdbbeb1ba739acb552440f72e1cc5ea1f
kirk <kirk@luno.com>
2020-01-13 16:19:51 +0200

    Refactor and include graph

--- johnny.go
+++ johnny.go
@@ -9,8 +9,10 @@ import (
 	"regexp"
 	"strings"
 
-	"github.com/go-echarts/go-echarts"
+	"github.com/chenjiandongx/go-echarts/charts"
 )
+
+var todos = map[string]int{}
 
 func main() {
 	args := os.Args[1:]
@@ -21,7 +23,6 @@ func main() {
 
 	start := args[0]
 	ext := args[1]
-	count := 0
 
 	err := filepath.Walk(start,
 		func(path string, info os.FileInfo, err error) error {
@@ -30,14 +31,10 @@ func main() {
 			}
 
 			if !info.IsDir() && strings.HasSuffix(path, ext) {
-				todos, err := findTodos(path)
+				err = findTodos(path)
 				if err != nil {
 					fmt.Println("File reading error", err)
 					return err
-				}
-
-				if len(todos) > 0 {
-					count += len(todos)
 				}
 			}
 			return nil
@@ -46,13 +43,39 @@ func main() {
 		log.Println(err)
 	}
 
-	fmt.Println("Total TODO's : ", count)
+	buildChart()
 }
 
-func findTodos(path string) ([]string, error) {
+func buildChart() {
+	names, nums := extractNames()
+	bar := charts.NewBar()
+	bar.SetGlobalOptions(charts.TitleOpts{Title: "TODOs"})
+	bar.AddXAxis(names)
+	bar.AddYAxis("Hover for names", nums)
+
+	f, err := os.Create("graph.html")
+	if err != nil {
+		log.Println(err)
+	}
+
+	bar.Render(f)
+}
+
+func extractNames() ([]string, []int) {
+	names := make([]string, 0, len(todos))
+	nums := make([]int, 0, len(todos))
+  for name, num := range todos {
+    names = append(names, name)
+		nums = append(nums, num)
+  }
+
+	return names, nums
+}
+
+func findTodos(path string) error {
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	defer func() {
@@ -61,16 +84,35 @@ func findTodos(path string) ([]string, error) {
 		}
 	}()
 
-	var todos []string
 	var reg = regexp.MustCompile(`(?m)^.*\/\/.*TODO(?:.*\\\s*\n)*.*`)
-	s := bufio.NewScanner(file)
 
+	s := bufio.NewScanner(file)
 	for s.Scan() {
 		todo := reg.FindString(s.Text())
 		if todo != "" {
-			todos = append(todos, todo)
+			dev := findDev(todo)
+			if dev == "" {
+				dev = "unclaimed"
+			}
+			todos[dev] += 1
 		}
 	}
 
-	return todos, s.Err()
+	return s.Err()
+}
+
+func findDev(s string) string {
+    i := strings.Index(s, "TODO(")
+		pad := 5
+		if i == -1 {
+			i = strings.Index(s, "TODO (")
+			pad = 6
+		}
+    if i >= 0 {
+        j := strings.Index(s[i:], ")")
+        if j >= 0 {
+            return s[i+pad : j+i]
+        }
+    }
+    return ""
 }
